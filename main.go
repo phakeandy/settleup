46e1f7894f01d833d6a9f874d7ffa9363113ddde
phakeandy <whota0@163.com>
2026-08-22 18:20:07 +0800

    update

--- main.go
+++ main.go
@@ -27,15 +27,12 @@ func main() {
 		w.Write([]byte("todo...\n"))
 	})
 
-	mux.HandleFunc("POST /api/orders", createOrderHandler)
+	mux.Handle("POST /api/orders", appHandler(createOrderHandler))
 	mux.Handle("GET /api/products", appHandler(listProductHandler))
 
 	if err := http.ListenAndServe(":8080", mux); err != nil {
 		slog.Error("fail to start", "error", err)
 	}
-}
-
-func createOrderHandler(w http.ResponseWriter, req *http.Request) {
 }
 
 type appHandler func(w http.ResponseWriter, r *http.Request) error
@@ -49,6 +46,50 @@ func (fn appHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		log.Printf("HTTP Error: %v", err)
 		http.Error(w, "An unexpected error occurred", http.StatusInternalServerError)
 	}
+}
+
+func createOrderHandler(w http.ResponseWriter, req *http.Request) error {
+	var payload struct {
+		// UserID         int    `json:"user_id"`
+		ProductID      int    `json:"product_id"`
+		Quantity       int64  `json:"quantity"`
+		IdempotencyKey string `json:"idempotency_key"`
+	}
+	if err := json.NewDecoder(req.Body).Decode(&payload); err != nil {
+		return err
+	}
+	if payload.Quantity <= 0 || payload.IdempotencyKey == "" || payload.ProductID == 0 {
+		return errors.New("invalid request payload")
+	}
+
+	var priceCent int64
+	if err := db.DB.QueryRow(`SELECT price_cent FROM products WHERE id = ?`, payload.ProductID).Scan(&priceCent); err != nil {
+		return err
+	}
+
+	amountCent := priceCent * payload.Quantity
+	res, err := db.DB.Exec(`INSERT INTO orders (user_id, product_id, quantity, amount_cent, status, idempotency_key) VALUES (-1, ?, ?, ?, 1, ?) `,
+		// payload.UserID, TODO: sigle user
+		payload.ProductID, payload.Quantity, amountCent, payload.IdempotencyKey)
+	if err != nil {
+		return err
+	}
+	orderID, err := res.LastInsertId()
+	if err != nil {
+		return err
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	return json.NewEncoder(w).Encode(H{
+		"id":              orderID,
+		// "user_id":         payload.UserID,
+		"user_id":         -1,
+		"product_id":      payload.ProductID,
+		"quantity":        payload.Quantity,
+		"amount_cent":     amountCent,
+		"status":          1,
+		"idempotency_key": payload.IdempotencyKey,
+	})
 }
 
 func listProductHandler(w http.ResponseWriter, req *http.Request) error {
